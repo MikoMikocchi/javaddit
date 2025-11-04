14bf7464d1d7803a11d12bb41d3a55a37ded0259
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-11-04 21:08:39 +0300

    initial commit

---
+++ src/main/java/com/example/javaddit/JavadditApplication.java
@@ -0,0 +1,11 @@
+package com.example.javaddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavadditApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/javaddit/JavadditApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.javaddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
